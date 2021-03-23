c58b1e5054f93f609b82fd1309d65b6c52ebca48
jonathanHuang876200 <jonathan876200@gmail.com>
2021-03-23 21:29:45 +0800

    Third

--- ITSA_practice/src/PokerCard.java
+++ ITSA_practice/src/PokerCard.java
@@ -20,58 +20,66 @@ public class PokerCard {
 				inputArrayList.add(sc.nextLine());
 			}
 		}
-		for (String string : inputArrayList) {
-			System.out.println(string);
-		}
 
-		String input = "D8 S3 D10 C12 H7 D5 S10 H8 H9";
+//		String input = "D8 S3 D10 C12 H7 D5 S10 H8 H9";
 //				String input = "D8 S3 D10 C12 H7";
 //				String input = "C5 D11 S1";
 //				String input = "H6 S3";
 //				String input = "H5 D4 S2 C13";
+		for (String inputStr : inputArrayList) {
+			String[] cards = inputStr.split("\\W+");
+			for (String card : cards) {
+				String capital = card.substring(0, 1);
+				int number = Integer.parseInt(card.substring(1));
+				if (capital.equals("S")) {
+					spadeNum.add(number);
+				}
+				if (capital.equals("H")) {
+					heartNum.add(number);
+				}
+				if (capital.equals("D")) {
+					diamondNum.add(number);
+				}
+				if (capital.equals("C")) {
+					clubNum.add(number);
+				}
 
-		String[] cards = input.split("\\W+");
-		for (String card : cards) {
-			String capital = card.substring(0, 1);
-			int number = Integer.parseInt(card.substring(1));
-			if (capital.equals("S")) {
-				spadeNum.add(number);
-			}
-			if (capital.equals("H")) {
-				heartNum.add(number);
-			}
-			if (capital.equals("D")) {
-				diamondNum.add(number);
-			}
-			if (capital.equals("C")) {
-				clubNum.add(number);
+				Collections.sort(spadeNum);
+				Collections.reverse(spadeNum);
+				Collections.sort(heartNum);
+				Collections.reverse(heartNum);
+				Collections.sort(diamondNum);
+				Collections.reverse(diamondNum);
+				Collections.sort(clubNum);
+				Collections.reverse(clubNum);
 			}
 
-			Collections.sort(spadeNum);
-			Collections.sort(heartNum);
-			Collections.sort(diamondNum);
-			Collections.sort(clubNum);
-		}
+			for (int number : spadeNum) {
+				String string = String.valueOf(number);
+				sortedList.add("S" + string);
+			}
+			for (int number : heartNum) {
+				String string = String.valueOf(number);
+				sortedList.add("H" + string);
+			}
+			for (int number : diamondNum) {
+				String string = String.valueOf(number);
+				sortedList.add("D" + string);
+			}
+			for (int number : clubNum) {
+				String string = String.valueOf(number);
+				sortedList.add("C" + string);
+			}
+			for (String s : sortedList) {
+				System.out.print(s + " ");
+			}
+			sortedList.clear();
+			spadeNum.clear();
+			heartNum.clear();
+			diamondNum.clear();
+			clubNum.clear();
+			System.out.println();
 
-		for (int number : spadeNum) {
-			String string = String.valueOf(number);
-			sortedList.add("S" + string);
-		}
-		for (int number : heartNum) {
-			String string = String.valueOf(number);
-			sortedList.add("H" + string);
-		}
-		for (int number : diamondNum) {
-			String string = String.valueOf(number);
-			sortedList.add("D" + string);
-		}
-		for (int number : clubNum) {
-			String string = String.valueOf(number);
-			sortedList.add("C" + string);
-		}
-
-		for (String s : sortedList) {
-			System.out.print(s.trim() + " ");
 		}
 
 	}
